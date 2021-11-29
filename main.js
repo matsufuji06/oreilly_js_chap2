258ff26b3a63463adc1d9e241fcf52ccc4d1acb5
matsufuji06 <n.matsufuji06@gmail.com>
2021-11-30 06:48:28 +0900

    toolオブジェクトを導入し、クリックしたところに円を表示

--- main.js
+++ main.js
@@ -4,13 +4,22 @@ $(document).ready(function() {
   paper.setup(document.getElementById('mainCanvas'));
 
   /* ここで描画する*/
-  let c;
-  for(let x=25; x<400; x+=50) {
-    for(let y=25; y<400; y+=50) {
-      let c = Shape.Circle(x, y, 20);
-      c.fillColor = 'green';
-    }
-  }
+
+  // ? 8*8で点を表示
+  // let c;
+  // for(let x=25; x<400; x+=50) {
+  //   for(let y=25; y<400; y+=50) {
+  //     let c = Shape.Circle(x, y, 20);
+  //     c.fillColor = 'green';
+  //   }
+  // }
+
+  let tool = new Tool();
+
+  tool.onMouseDown = function(event) {
+    let c = Shape.Circle(event.point.x, event.point.y, 20);
+    c.fillColor = 'green';
+  };
 
   paper.view.draw();
   console.log('読み込み完了。')
