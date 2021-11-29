66efa3b0dc30d4a9315974fe17983904a84a2bd3
matsufuji06 <n.matsufuji06@gmail.com>
2021-11-30 06:40:26 +0900

    for文で点を繰り返し表示

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ $(document).ready(function() {
   paper.setup(document.getElementById('mainCanvas'));
 
   /* ここで描画する*/
-  let c = Shape.Circle(200, 200, 50);
-  c.fillColor = 'green';
+  let c;
+  for(let x=25; x<400; x+=50) {
+    for(let y=25; y<400; y+=50) {
+      let c = Shape.Circle(x, y, 20);
+      c.fillColor = 'green';
+    }
+  }
 
   paper.view.draw();
   console.log('読み込み完了。')
